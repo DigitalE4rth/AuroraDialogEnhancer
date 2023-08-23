a9d9cafce11ef7052bb6c61020b3a2893d18c952
DigitalE4rth <rudkovskiy.den@yandex.ru>
2023-08-23 14:53:24 +0300

    New default primary color

--- AuroraDialogEnhancer/Properties/DefaultSettings.Designer.cs
+++ AuroraDialogEnhancer/Properties/DefaultSettings.Designer.cs
@@ -12,7 +12,7 @@ namespace AuroraDialogEnhancer.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.5.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.6.0.0")]
     internal sealed partial class DefaultSettings : global::System.Configuration.ApplicationSettingsBase {
         
         private static DefaultSettings defaultInstance = ((DefaultSettings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new DefaultSettings())));
@@ -25,7 +25,7 @@ namespace AuroraDialogEnhancer.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("#7764d8")]
+        [global::System.Configuration.DefaultSettingValueAttribute("#FFD7A200")]
         public string UI_ThemeInfo_AccentColor {
             get {
                 return ((string)(this["UI_ThemeInfo_AccentColor"]));
