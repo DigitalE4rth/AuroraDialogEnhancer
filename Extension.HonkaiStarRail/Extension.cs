3a7bc90ce51397878ee22d5d197351b71628b98b
DigitalE4rth <rudkovskiy.den@yandex.ru>
2023-08-24 12:10:13 +0300

    Feature: Extensions use reflection to set assembly versions

--- Extension.HonkaiStarRail/Extension.cs
+++ Extension.HonkaiStarRail/Extension.cs
@@ -14,7 +14,7 @@ public sealed class Extension : ExtensionDto
 
     public override string Author { get; protected set; } = "E4rth";
 
-    public override string Version { get; protected set; } = "1.0.0.0";
+    public override string Version { get; protected set; } = typeof(Extension).Assembly.GetName().Version.ToString();
 
     public override string Link { get; protected set; } = "https://gitee.com/e4rth/aurora-dialog-enhancer";
 
