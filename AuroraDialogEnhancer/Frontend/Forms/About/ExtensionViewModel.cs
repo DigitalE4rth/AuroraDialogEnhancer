123ade85ba7d204cc0111da5e2f01a0770bc8556
DigitalE4rth <rudkovskiy.den@yandex.ru>
2023-08-24 12:53:53 +0300

    Unused imports and spaces

--- AuroraDialogEnhancer/Frontend/Forms/About/ExtensionViewModel.cs
+++ AuroraDialogEnhancer/Frontend/Forms/About/ExtensionViewModel.cs
@@ -1,23 +1,19 @@
 ﻿using AuroraDialogEnhancerExtensions;
-using AuroraDialogEnhancerExtensions.Content;
 
 namespace AuroraDialogEnhancer.Frontend.Forms.About;
 
 public class ExtensionViewModel
 {
-    public string? Title { get; set; }
-
+    public string? Title       { get; set; }
     public string? Description { get; set; }
-
-    public string? Author { get; set; }
-
-    public string? Link { get; set; }
+    public string? Author      { get; set; }
+    public string? Link        { get; set; }
 
     public ExtensionViewModel(ExtensionDto extension)
     {
-        Title = extension.DisplayName;
+        Title       = extension.DisplayName;
         Description = $"{extension.Version} • {extension.Author}";
-        Author = extension.Author;
-        Link = extension.Link;
+        Author      = extension.Author;
+        Link        = extension.Link;
     }
 }
