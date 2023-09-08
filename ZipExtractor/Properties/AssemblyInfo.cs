f840c863eca68f15b71ca6e81349ea6f9e005c8e
DigitalE4rth <rudkovskiy.den@yandex.ru>
2023-09-08 09:35:46 +0300

    Removed description in zip extractor

--- ZipExtractor/Properties/AssemblyInfo.cs
+++ ZipExtractor/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Updater")]
-[assembly: AssemblyDescription("ZipExtractor")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Aurora Works")]
 [assembly: AssemblyProduct("Updater")]
