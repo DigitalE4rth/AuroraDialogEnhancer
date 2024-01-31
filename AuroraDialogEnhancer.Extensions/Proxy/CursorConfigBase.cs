d72abd530fbc01e28638d799d9013687adbfb339
DigitalE4rth <rudkovskiy.den@yandex.ru>
2024-01-31 11:39:21 +0300

    CursorConfigBase adjustments

--- AuroraDialogEnhancer.Extensions/Proxy/CursorConfigBase.cs
+++ AuroraDialogEnhancer.Extensions/Proxy/CursorConfigBase.cs
@@ -5,8 +5,8 @@ namespace AuroraDialogEnhancerExtensions.Proxy;
 public abstract class CursorConfigBase
 {
     public virtual DynamicPoint InitialPosition       { get; set; } = new (0.15, 0.85);
-    public virtual int          InitialPositionX      { get; set; } = 3;
-    public virtual int          HiddenCursorPositionY { get; set; }
-    public virtual double       PlacementSmoothness   { get; set; } = 0.2;
-    public virtual double       MovementSmoothness    { get; set; } = 10;
+    public virtual int          InitialPositionX      { get; set; } 
+    public virtual int          HiddenCursorPositionY { get; set; } = 3;
+    public virtual double       PlacementSmoothness   { get; set; } = 0.1;
+    public virtual double       MovementSmoothness    { get; set; } = 3;
 }
--- Extension.HonkaiStarRail/Presets/CursorConfig.cs
+++ Extension.HonkaiStarRail/Presets/CursorConfig.cs
@@ -5,4 +5,5 @@ namespace Extension.HonkaiStarRail.Presets;
 public class CursorConfig : CursorConfigBase
 {
     public override int HiddenCursorPositionY => 1;
+    public override double PlacementSmoothness => 0.2;
 }
